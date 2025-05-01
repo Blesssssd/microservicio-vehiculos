17511115358e17adc4f3a514e1e6a32a0dc388e1
Blesssssd <potes.juan.0239@eam.edu.co>
2025-04-30 21:14:35 -0500

    feat: agregar ruta '/' con Hola Mundo y workflow de despliegue desde Docker Hub

--- cmd/main.go
+++ cmd/main.go
@@ -4,32 +4,38 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
 	"github.com/joho/godotenv"
+	"github.com/gorilla/mux"
 
 	"github.com/Blesssssd/microservicio-golang/config"
-	"github.com/gorilla/mux"
-    "github.com/Blesssssd/microservicio-golang/controllers"
-    "github.com/Blesssssd/microservicio-golang/repository"
+	"github.com/Blesssssd/microservicio-golang/controllers"
+	"github.com/Blesssssd/microservicio-golang/repository"
 )
 
 func main() {
-    godotenv.Load()
-    client, collection := config.ConnectMongo()
-    defer client.Disconnect(nil)
+	godotenv.Load()
+	client, collection := config.ConnectMongo()
+	defer client.Disconnect(nil)
 
-    fmt.Println("✅ Conectado a MongoDB")
+	fmt.Println("✅ Conectado a MongoDB")
 
-    repo := repository.NewVehicleRepository(collection)
-    ctrl := controllers.NewVehicleController(repo)
+	repo := repository.NewVehicleRepository(collection)
+	ctrl := controllers.NewVehicleController(repo)
 
-    r := mux.NewRouter()
+	r := mux.NewRouter()
 
-    r.HandleFunc("/vehicles", ctrl.CreateVehicle).Methods("POST")
-    r.HandleFunc("/vehicles", ctrl.GetVehicles).Methods("GET")
-    r.HandleFunc("/vehicles/{id}", ctrl.GetVehicleByID).Methods("GET")
-    r.HandleFunc("/vehicles/{id}", ctrl.UpdateVehicle).Methods("PUT")
-    r.HandleFunc("/vehicles/{id}", ctrl.DeleteVehicle).Methods("DELETE")
+	// ✅ Ruta adicional para "Hola Mundo"
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hola Mundo")
+	}).Methods("GET")
 
-    fmt.Println("🚗 Servidor corriendo en http://localhost:8080")
-    log.Fatal(http.ListenAndServe(":8080", r))
+	r.HandleFunc("/vehicles", ctrl.CreateVehicle).Methods("POST")
+	r.HandleFunc("/vehicles", ctrl.GetVehicles).Methods("GET")
+	r.HandleFunc("/vehicles/{id}", ctrl.GetVehicleByID).Methods("GET")
+	r.HandleFunc("/vehicles/{id}", ctrl.UpdateVehicle).Methods("PUT")
+	r.HandleFunc("/vehicles/{id}", ctrl.DeleteVehicle).Methods("DELETE")
+
+	fmt.Println("🚗 Servidor corriendo en http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
